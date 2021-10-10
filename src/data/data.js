c2c12d00cf2ded7bf37eca69e0ff574e08173411
Sedulla <67521243+Sedulla@users.noreply.github.com>
2021-10-10 11:26:25 +0400

    Build Slider of Home Page

---
+++ src/data/data.js
@@ -0,0 +1,20 @@
+export const slideItems = [
+  {
+    id: 1,
+    image: 'https://i.ibb.co/XsdmR2c/1.png',
+    title: 'LOREM IPSUM',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+  },
+  {
+    id: 2,
+    image: 'https://i.ibb.co/XsdmR2c/1.png',
+    title: 'LOREM IPSUM',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+  },
+  {
+    id: 3,
+    image: 'https://i.ibb.co/XsdmR2c/1.png',
+    title: 'LOREM IPSUM',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+  },
+];
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Products from '../components/Products'
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Slider from '../components/Slider';
+import Products from '../components/Products';
 
 const Home = () => {
-    return (
-        <div>
-            <Navbar/>
-            <Products/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Navbar />
+      <Slider />
+      <Products />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
