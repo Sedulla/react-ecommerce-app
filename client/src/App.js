3be446fd9d7598ac4730d50abf0d2e8b70c2f1f8
Sedulla <67521243+Sedulla@users.noreply.github.com>
2021-10-21 15:48:45 +0400

    Build Announcement Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,11 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
-import Product from './pages/ProductPage';
-import Login from './pages/Login';
-import Register from './pages/Register';
+// import Product from './pages/ProductPage';
+// import Login from './pages/Login';
+// import Register from './pages/Register';
 
 function App() {
   return (
-    <Router>
-      <App />
-    </Router>
+      <Home />
   );
 }
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,10 +2,12 @@ import Nav from '../components/Nav';
 import Slider from '../components/Slider';
 import Products from '../components/Products';
 import Footer from '../components/Footer';
+import Announcement from '../components/Announcement';
 
 const Home = () => {
   return (
     <div>
+      <Announcement />
       <Nav />
       <Slider />
       <Products />
