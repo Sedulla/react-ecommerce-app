c20f1e212fbf918be147ba6bdfbe126cc2043295
Sedulla <67521243+Sedulla@users.noreply.github.com>
2022-03-27 18:35:06 +0400

    build: change route urls

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,29 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 // Pages and components
 import { Homepage } from './views/Homepage';
-import { LogIn } from './views/LogIn';
-import { SignUp } from './views/SignUp';
 import { SingleProduct } from './views/SingleProduct';
 import { ProductList } from './views/ProductList';
 import { Cart } from './views/Cart';
+import { LogIn } from './views/LogIn';
+import { SignUp } from './views/SignUp';
 // import ProtectedRoute from './pages/ProtectedRoute';
 
 export const App = () => {
+  const user = true;
+
   return (
     <>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/login" element={<LogIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/product" element={<SingleProduct />} />
-        <Route path="/collection" element={<ProductList />} />
+        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="/product/:id" element={<SingleProduct />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={user ? <Navigate to="/" replace /> : <LogIn />} />
+        <Route
+          path="/signup"
+          element={user ? <Navigate to="/" replace/> : <SignUp />}
+        />
         <Route path="*" element={<div>404 Not Found</div>} />
       </Routes>
     </>
