592ecc1e5a53b206f8886e3301af16633ebd2ddd
Sedulla <67521243+Sedulla@users.noreply.github.com>
2022-05-07 12:22:45 +0400

    new commit message

--- app/client/src/redux/cartSlice.js
+++ app/client/src/redux/cartSlice.js
@@ -22,7 +22,7 @@ const cartSlice = createSlice({
       //   state.products.push({ ...product, quantity });
       // } else {
       //   state.products[productIndex].quantity += quantity;
-      // }
+      //
     },
     removeProduct: (state, action) => {
       const { product, quantity } = action.payload;
